3d18d25df8d13deb05eb6039c03d2f57d1f4985c
Cristina Rodriguez <cristina.rodriguez.t@gmail.com>
2024-12-05 09:51:36 +0000

    Day 5 part 2

--- src/day05.rs
+++ src/day05.rs
@@ -32,8 +32,10 @@ pub fn day05(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std
     // println!("{:?}", page_order);
     // println!("{:?}", print_manuals);
 
-    let mut middle_pages_sum = 0;
+    let mut ordered_middle_pages_sum = 0;
+    let mut disordered_middle_pages_sum = 0;
     for manual in print_manuals {
+        // println!("Manual: {:?}", manual);
         let mut is_ordered = true;
         for order in &page_order {
             if manual.contains(&order.0) & manual.contains(&order.1) {
@@ -50,11 +52,39 @@ pub fn day05(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std
             // now we need to find the middle point
             //assuming odd length
             let mid_point = (manual.len()-1)/2;
-            middle_pages_sum += manual[mid_point];
+            ordered_middle_pages_sum += manual[mid_point];
+        }
+        else {
+            // first need to reorder them
+
+            let mut reordered = false;
+            let mut ordering_manual = manual.clone();
+            while !reordered {
+                reordered = true;
+                for order in &page_order {
+                    if ordering_manual.contains(&order.0) & ordering_manual.contains(&order.1) {
+                        // check
+                        let first_i = ordering_manual.iter().position(|&x| x == order.0).unwrap();
+                        let second_i = ordering_manual.iter().position(|&x| x == order.1).unwrap();
+                        if first_i > second_i {
+                            // swap then break and repeat
+                            ordering_manual.swap(first_i, second_i);
+                            reordered = false;
+                            break;
+                        }
+                    }
+                }
+            }// reordering now
+            // it is in order so, now we can find the mid point again
+            // println!("reordering attempt: {:?}", ordering_manual);
+            let mid_point = (ordering_manual.len()-1)/2;
+            disordered_middle_pages_sum += ordering_manual[mid_point];
         }
     }
-    println!("Middle pages sum is: {}", middle_pages_sum);
+    println!("Ordered middle pages sum is: {}", ordered_middle_pages_sum);
     // 5329
+    println!("Disordered middle pages sum is: {}", disordered_middle_pages_sum);
+    // 5833
 
     Ok(())
 }
