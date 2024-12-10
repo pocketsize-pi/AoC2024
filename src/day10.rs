bb897bf34c833f7beb4b1708618bb1a21d6cb18b
Cristina Rodriguez <cristina.rodriguez.t@gmail.com>
2024-12-10 14:04:44 +0000

    Day 10 part 2
    In which making mistakes in part 1 pays off incredibly!

--- src/day10.rs
+++ src/day10.rs
@@ -20,13 +20,13 @@ pub fn day10(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std
         }
     }
 
-    let mut num_trails = 0;
+    let mut num_trails;
     let mut trail_score = 0;
 
     let trail_dirs = [Direction::North, Direction::East, Direction::South, Direction::West];
 
-    for start_pos in start_positions {
-        let mut trail_queue = vec![start_pos];
+    for start_pos in &start_positions {
+        let mut trail_queue = vec![*start_pos];
         let mut visited_positions = Vec::new();
         num_trails = 0;
 
@@ -62,6 +62,45 @@ pub fn day10(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std
     println!("There total score is {}", trail_score );
     // 574 first try
 
+
+    // I accidentally coded part 2 earlier, by forgetting to add to visited path
+    trail_score = 0;
+    for start_pos in &start_positions {
+        let mut trail_queue = vec![*start_pos];
+        let mut visited_positions = Vec::new();
+        num_trails = 0;
+
+        while !trail_queue.is_empty() {
+            let current_pos = trail_queue.pop().unwrap();
+            visited_positions.push(current_pos);
+
+            let current_slope = topological_map.get(&current_pos).unwrap();
+
+            // find all adjacent points
+            for dir in trail_dirs {
+                let mut new_point = current_pos;
+                new_point.move_one(&dir);
+                if topological_map.contains_key(&new_point) {
+                    let new_slope = topological_map.get(&new_point).unwrap();
+                    if *new_slope == (current_slope + 1) {
+                        // good point, but have we see it yet?
+                        // if !visited_positions.contains(&new_point) {
+                            if *new_slope == 9 {
+                                num_trails += 1;
+                            }
+                            // we should add it regardless, so that we don't check the 9s again
+                            trail_queue.push(new_point);
+                        // }
+                    }
+                }
+            }
+        }
+        // end of a trail head, let's add the score
+        // println!("Current trails: {}", num_trails);
+        trail_score +=  num_trails;
+    }
+    println!("There total score is {}", trail_score );
+    // 1238 yes! I had indeed coded it earlier
 
     Ok(())
 }
