913ec9afe6d8d1789a1083831c14a2bf47dbcd98
Cristina Rodriguez <cristina.rodriguez.t@gmail.com>
2024-12-17 19:50:25 +0000

    Day 14 pt 2 attempts
    So far no dice

--- src/day14.rs
+++ src/day14.rs
@@ -1,4 +1,5 @@
 use aoc_2024::*;
+use press_btn_continue;
 
 pub fn day14(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std::error::Error>> {
     println!("Day 14");
@@ -37,8 +38,8 @@ pub fn day14(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std
     // println!();
 
 
-    let wait = 100;
-    for _i in 0..wait {
+    let wait = i32::MAX;
+    for i in 1..wait {
         for robot_id in 0..fleet_pos.len() {
             let robot_pos = &mut fleet_pos[robot_id];
             let robot_vel = fleet_vel[robot_id];
@@ -49,34 +50,82 @@ pub fn day14(input_type: InputType, manual_name: &str) -> Result<(), Box<dyn std
             // ah! I forgot when it goes negative!
             if robot_pos.c_x >= width {
                 robot_pos.c_x = robot_pos.c_x % width;
-            }
-            else if robot_pos.c_x < 0 {
+            } else if robot_pos.c_x < 0 {
                 robot_pos.c_x = width - robot_pos.c_x.abs();
             }
             if robot_pos.r_y >= height {
                 robot_pos.r_y = robot_pos.r_y % height;
-            }
-            else if robot_pos.r_y < 0 {
+            } else if robot_pos.r_y < 0 {
                 robot_pos.r_y = height - robot_pos.r_y.abs();
             }
             // println!("new pos: {:?}", robot_pos);
             // I bet we can do this by multiplying by 100 and then just straight up %
         }
 
-        // for r in 0..height {
-        //     for c in 0..width {
-        //         let pt = Point {r_y: r, c_x: c};
-        //         if fleet_pos.contains(&pt) {
-        //             let num = fleet_pos.iter().filter(|robot| **robot == pt).count();
-        //             print!("{num}");
-        //         }
-        //         else {
-        //             print!(".");
-        //         }
-        //     }
-        //     println!();
-        // }
-        // println!();
+        if i >= 2017243 {
+
+        // surely the three will be symmetrical
+
+        let top_half = (height - 1) / 2;
+        let bottom_half = height - top_half; // this should bypass the middle
+        let left_half = (width - 1) / 2;
+        let right_half = width - left_half;
+        let top_left = fleet_pos.iter().filter(|robot| (robot.c_x < left_half) & (robot.r_y < top_half)).collect::<Vec<&Point>>().len();
+        let top_right = fleet_pos.iter().filter(|robot| (robot.c_x >= right_half) & (robot.r_y < top_half)).collect::<Vec<&Point>>().len();
+        let bottom_left = fleet_pos.iter().filter(|robot| (robot.c_x < left_half) & (robot.r_y >= bottom_half)).collect::<Vec<&Point>>().len();
+        let bottom_right = fleet_pos.iter().filter(|robot| (robot.c_x >= right_half) & (robot.r_y >= bottom_half)).collect::<Vec<&Point>>().len();
+
+        // println!("{} {} {} {}", top_half, bottom_half, left_half, right_half);
+        // println!("{} {} {} {}", top_left, top_right, bottom_left, bottom_right);
+        // println!("Safety factor: {}", top_left* top_right* bottom_left* bottom_right);
+
+
+        if ((top_left == top_right) & (bottom_left == bottom_right)) | ((top_left == bottom_left) & (top_right == bottom_right)) {
+            let mut mega_string = "".to_owned();
+            let mut line = false;
+            for r in 0..height {
+                for c in 0..width {
+                    let pt = Point { r_y: r, c_x: c };
+                    if fleet_pos.contains(&pt) {
+                        let num = fleet_pos.iter().filter(|robot| **robot == pt).count() as u8;
+                        // print!("{num}");
+                        mega_string.push(char::from_digit(num as u32, 10).unwrap());
+                    } else {
+                        // print!(".");
+                        mega_string.push('.');
+                    }
+                }
+                // println!();
+                mega_string.push("\n".parse().unwrap());
+                if mega_string.contains("1111") {
+                    line = true;
+                }
+            }
+            if line {
+                // println!("{:?}", mega_string);
+                println!();
+
+                for r in 0..height {
+                    for c in 0..width {
+                        let pt = Point {r_y: r, c_x: c};
+                        if fleet_pos.contains(&pt) {
+                            let num = fleet_pos.iter().filter(|robot| **robot == pt).count();
+                            print!("{num}");
+                        }
+                        else {
+                            print!(".");
+                        }
+                    }
+                    println!();
+                }
+                println!();
+
+                println!("Clicks: {i}");
+                press_btn_continue::wait("Picture?").unwrap();
+            }
+
+        }
+    }
     }
 
 
